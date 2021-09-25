24ca956c69ba15bbe3b7fe1549ac10c58e69b94c
Kan-Ru Chen <kanru@kanru.info>
2021-09-25 15:45:30 +0900

    refactor: use char instead string for single char pattern

--- src/worker.rs
+++ src/worker.rs
@@ -213,7 +213,7 @@ fn is_hidden(entry: &DirEntry) -> bool {
     entry
         .file_name()
         .to_str()
-        .map(|s| s.starts_with("."))
+        .map(|s| s.starts_with('.'))
         .unwrap_or(false)
 }
 
